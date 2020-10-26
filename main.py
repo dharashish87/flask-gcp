6a2e68b9fc338064acb468756368b4ffe82497e8
Ashish Dhar <dhar.ashish87@gmail.com>
2020-10-26 12:46:21 +0530

    nlp change

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect, jsonify
 
 app = Flask(__name__)
 
@@ -9,6 +9,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/nlp")
+def hello():
+    #return request.args.get("query")
+    return jsonify(
+        message = request.args.get("query"),
+        category1 = "Category1",
+        category2 = "Category2",
+        category3 = "Category3",
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
